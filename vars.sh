2086ef2a29fb56fb394dd8c730c97afd837e0b54
Jeffrey L. Roberts <Jeffrey.L.Roberts@gmail.com>
2014-01-06 02:49:11 -0500

    Updated vars script to include newest version of VBox Guest Additions

--- vars.sh
+++ vars.sh
@@ -1,6 +1,6 @@
 NAME=centos65-x86_64
 TYPE=RedHat_64
 INSTALLER="./isos/CentOS-6.5-x86_64-minimal.iso"
-GUESTADDITIONS="./isos/VBoxGuestAdditions-4.3.4.iso"
+GUESTADDITIONS="./isos/VBoxGuestAdditions-4.3.6.iso"
 HDD="${HOME}/VirtualBox VMs/${NAME}/${NAME}.vmdk"
 NATNET=10.0.2.0/24
